865553f3864617bdeafd2a05a2f3cf5a37abe8c7
sinnisingla <sinnisingla@outlook.com>
2022-04-26 09:36:52 +0530

    feat: add books controller

---
+++ SecurityDemoMultiAuth/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SecurityDemoMultiAuth.Models
+{
+    public partial class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ SecurityDemoMultiAuth/Models/MyBooksDBContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace SecurityDemoMultiAuth.Models
+{
+    public partial class MyBooksDBContext : DbContext
+    {
+        public MyBooksDBContext()
+        {
+        }
+
+        public MyBooksDBContext(DbContextOptions<MyBooksDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MyBooksDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
